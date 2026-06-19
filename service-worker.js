c985c645ccc7135c7a4d83be3130e4287de52042
lexington1988 <lexington88@hotmail.co.uk>
2026-06-19 22:28:22 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "spanish-islands-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./icon-32x32.png",
+  "./apple-touch-icon.png",
+  "./icon-192x192.png",
+  "./icon-512x512.png",
+  "./icon-maskable-192x192.png",
+  "./icon-maskable-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
